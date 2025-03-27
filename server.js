011acaf7b92d040f29e75072c5f0cd43e2caa1d8
AHMED MUKHTAR <AAMEDMUKHTAR@GMAIL.com>
2025-03-28 00:41:29 +0200

    secound commit

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ app.use(express.static('uploads'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 // // استخدام المكونات المتوسطة (middleware) لتحليل البيانات
-Application.use(express.urlencoded({ extended: false }));
-Application.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 
 
