e844f927b6e1d3a1fbef4fdfdf8a19ca3378e7f5
AHMED MUKHTAR <AAMEDMUKHTAR@GMAIL.com>
2025-03-28 00:35:44 +0200

    secound commit

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require("path");
 const fs = require("fs");
 const app = express();
 const port = process.env.PORT || 1000;
+require('dotenv').config();
 const upload = multer({ storage: multer.memoryStorage() });
 app.set("view engine", "ejs");
 app.use(express.static("public"));
